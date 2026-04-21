4bce61b6449eff222bc59cc722439f6f1d4deded
Dmytro Humeniuk <269216105+melmonreaz@users.noreply.github.com>
2026-04-21 16:17:29 +0300

    Implement caching decorator to optimize function calls

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    memory = {}
+
+    def wrapper(*args, **kwargs) -> int:
+        # nonlocal memory
+        if args not in memory:
+            memory[args] = func(*args, **kwargs)
+            print("Calculating new result")
+            return memory[args]
+        print("Getting from cache")
+        return memory[args]
+
+    return wrapper
